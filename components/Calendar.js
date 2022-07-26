498dc933b266c327a076ca22c828e642a53c7f4a
proximahaiz@gmail.com <karpov.volodymyr.v@gmail.com>
2022-07-26 20:15:30 +0300

    update(Login): move login logic from App-> Login js

--- components/Calendar.js
+++ components/Calendar.js
@@ -1,9 +1,46 @@
-import {Text} from 'react-native';
+import {Button} from 'react-native';
 
 
 const Calendar = () => {
+    const config = {
+        "clientId": "382648074525-3bh0jsq03ut910i367er4djqp2fm0iok.apps.googleusercontent.com",
+        "apiKey": "AIzaSyBj-fHnlIw5LSBCk3yJrDKf-4grjW3leGE",
+        "scope": "https://www.googleapis.com/auth/calendar",
+        "discoveryDocs": [
+            "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"
+        ]
+    }
+
+    const getEvents = () => {
+        const CALENDAR_ID = 'someCalendarID';
+        const API_KEY = 'AIzaSyBj-fHnlIw5LSBCk3yJrDKf-4grjW3leGE';
+        const beginDate = new Date();
+        let url = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${API_KEY}&timeMin=${beginDate.toISOString()}&maxResults=50&singleEvents=true&orderBy=startTime`;
+
+        fetch(url)
+            .then((response) => response.json())
+            .then((responseJson) => {
+                console.log('responseJson', responseJson);
+                // this.setState({
+                //     pageToken: responseJson.nextPageToken,
+                //     dataSource: [...this.state.dataSource, ...responseJson.items],
+                //     loading: false,
+                //     refreshing: false,
+                //     error: responseJson.error || null,
+                // });
+            })
+            .then(() => {
+                // this.getDates()
+            })
+            .catch(error => {
+                this.setState({error, loading: false, refreshing: false});
+            });
+    };
+
+
     return (
-        <Text>Calendar</Text>
+        <Button onPress={() => getEvents()} title="Get events">
+        </Button>
     )
 }
 
--- components/Login.js
+++ components/Login.js
@@ -1,10 +1,60 @@
-import {Text} from 'react-native';
+import {Button, StatusBar, StyleSheet, Text, View} from 'react-native';
+import * as Google from 'expo-auth-session/providers/google';
+import {useEffect, useState} from 'react';
 
 const Login = () => {
+    const [accessToken, setAccessToken] = useState()
+    const [userInfo, setUserInfo] = useState()
 
+    const [request, response, promptAsync] = Google.useAuthRequest({
+        iosClientId: "382648074525-3bh0jsq03ut910i367er4djqp2fm0iok.apps.googleusercontent.com",
+        androidClientId: "382648074525-4ll05bhmc1od6ubeu4n4ri2omjhs88mj.apps.googleusercontent.com",
+        expoClientId: "382648074525-luus5lp62g7f13fjsq8h19poe2541ltr.apps.googleusercontent.com"
+    })
+
+    useEffect(() => {
+        if (response?.type === 'success') {
+            setAccessToken(response.authentication.accessToken);
+        }
+    }, [response])
+
+    async function getUserData() {
+        let userInfoResponse = await fetch("https://www.googleapis.com/userinfo/v2/me", {
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        })
+
+        userInfoResponse.json().then(data => setUserInfo(data))
+    }
+
+    const showUserInfo = () => {
+        if (userInfo) {
+            return (
+                <View>
+                    <Text>Welcome {userInfo.name}</Text>
+                </View>
+            )
+        }
+    }
     return (
-        <Text>Login</Text>
+            <View style={styles.container}>
+                {showUserInfo()}
+                <Button onPress={accessToken ? getUserData : () => {
+                    promptAsync({showInRecents: true})
+                }} title={accessToken ? "Get User Data" : "Login"}/>
+                <StatusBar style="auto"/>
+            </View>
     )
 }
 
 export default Login
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
