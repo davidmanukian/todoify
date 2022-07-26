612c352ca3bd3e091ea52ab63e94722561099f47
proximahaiz@gmail.com <karpov.volodymyr.v@gmail.com>
2022-07-26 23:07:23 +0300

    update(Calendar): add working config

--- components/Calendar.js
+++ components/Calendar.js
@@ -1,4 +1,5 @@
 import {Button} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 const Calendar = () => {
@@ -11,13 +12,40 @@ const Calendar = () => {
         ]
     }
 
+
+    const getData = async () => {
+        try {
+            const value = await AsyncStorage.getItem('accessToken')
+            if (value !== null) {
+                console.log(value);
+                return value;
+                // value previously stored
+            }
+        } catch (e) {
+            // error reading value
+        }
+    }
+
+    const localAccessToken =  'ya29.A0AVA9y1thrXBYUTgUi0iT_mROrgeunUVOtiug1Oa_e2i6NoTKTO5t1bQDIjMArX_VZJWtyyP3Un_T775Atw-CtPTgd38puEUGLnZ2E5vy1znPdeJKgFP586MKhtf8kMsDBMbVLhVMa5M0781E7Z6HihtH4sXhYUNnWUtBVEFTQVRBU0ZRRTY1ZHI4dnNrd3FSWjZIZmRubHlNWWVQVWU4dw0163'
+    // const localAccessToken =  'ya29.A0AVA9y1ti48xvfl2_91LMgtQzB97u67iMjzI5RcCr9bFVKaZ0q9Ue2En-l7_jR1MU5PxmvN0iXzWLqzCLp5JZh3wicrwBijVvElMGRT7Z8v5kfYUZDKElpnYfZV8MNp4sUO1IwibR3sJ-Omn2aMqgrnzY0tI68YKmMjSV-fsYUNnWUtBVEFTQVRBU0ZRRTY1ZHI4X2ZlbUVUUXBwQWRfTHJFQk42NFhOZw0174'
+    console.log('localAccessToken', localAccessToken);
+
+    // getData().
+
     const getEvents = () => {
         const CALENDAR_ID = 'someCalendarID';
         const API_KEY = 'AIzaSyBj-fHnlIw5LSBCk3yJrDKf-4grjW3leGE';
         const beginDate = new Date();
-        let url = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${API_KEY}&timeMin=${beginDate.toISOString()}&maxResults=50&singleEvents=true&orderBy=startTime`;
+        // let url = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${API_KEY}&timeMin=${beginDate.toISOString()}&maxResults=50&singleEvents=true&orderBy=startTime`;
+        let url = `https://www.googleapis.com/calendar/v3/calendars/primary/events`;
 
-        fetch(url)
+
+        fetch(url,
+            {
+                headers: {
+                    Authorization: `Bearer ${localAccessToken}`
+                }
+            })
             .then((response) => response.json())
             .then((responseJson) => {
                 console.log('responseJson', responseJson);
