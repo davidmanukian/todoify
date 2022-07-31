195f2b424dcc4b41c8880a258c6289771081ff3b
proximahaiz@gmail.com <karpov.volodymyr.v@gmail.com>
2022-07-31 16:28:23 +0300

    update(): update logout on Settings page

--- components/Settings.js
+++ components/Settings.js
@@ -1,28 +1,14 @@
 import {Button} from 'react-native';
-import * as WebBrowser from 'expo-web-browser';
+import {useAuth} from '../hooks/auth';
 
 const Settings = () => {
-    const logout = () => {
-        WebBrowser.dismissAuthSession();
-        console.log('logout');
-        // AuthSession.revokeAsync({
-        //     token: accessToken
-        // }).then(r => {
-        //     console.log(r);
-        // })
-    }
+    const {signOut} = useAuth()
 
 
     return (
         <Button
             title="Log out"
-            onPress={async () => {
-                setUserInfo(null);
-                setAccessToken(null);
-                window.location.replace(
-                    `https://www.googleapis.com/v2/logout?client_id=${iosClientId}&returnTo=${'/'}`
-                );
-            }}
+            onPress={() => signOut()}
         />
     )
 }
