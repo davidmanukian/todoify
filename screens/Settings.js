b7b30fc61771fbd2226a63e6d1dbdcc77f9ca296
proximahaiz@gmail.com <karpov.volodymyr.v@gmail.com>
2022-08-07 19:46:25 +0300

    update(): add swipe list

--- screens/Settings.js
+++ screens/Settings.js
@@ -3,9 +3,11 @@ import {
     Dimensions,
     KeyboardAvoidingView,
     Platform,
-    ScrollView,
     StyleSheet,
+    Text,
     TextInput,
+    TouchableHighlight,
+    TouchableOpacity,
     View
 } from 'react-native';
 import {useAuth} from '../hooks/auth';
@@ -14,6 +16,7 @@ import {useStorage} from '../hooks/storage';
 import {COLLECTION_SECTIONS} from '../constant_storage';
 import {Cell, Section, TableView} from 'react-native-tableview-simple';
 import app_constants from '../app_constants';
+import {SwipeListView} from 'react-native-swipe-list-view';
 
 const {
     width: SCREEN_WIDTH,
@@ -54,6 +57,11 @@ const Settings = () => {
         setSectionName('');
     }
 
+    const deleteSection = (index) => {
+        sections.slice(index, 1)
+        persistSections([...sections.filter((c, innerI) => innerI !== index)]);
+    }
+
     const persistSections = (list) => {
         storeItem(COLLECTION_SECTIONS, list);
         setSections(list);
@@ -69,6 +77,7 @@ const Settings = () => {
     useEffect(() => {
         getItem(COLLECTION_SECTIONS).subscribe((c) => {
             if (c.length >= 1) {
+                console.log('set sections', c.length);
                 setSections(c);
             }
         })
@@ -86,22 +95,52 @@ const Settings = () => {
                     title="Add New Section"
                 />
             </Section>
-            <ScrollView contentContainerStyle={styles.stage}>
-                <TableView>
-                    <Section header="Sections">
-                        {
-                            sections && sections.length >= 1 ? sections?.map((s, index) => {
-                                return <Cell
-                                    key={index}
-                                    onPress={() => changeAvailability(index)}
-                                    accessory={s?.isSelected ? 'Checkmark' : null}
-                                    title={s?.name}
-                                />
-                            }) : ''
-                        }
-                    </Section>
-                </TableView>
-            </ScrollView>
+            {/*<ScrollView contentContainerStyle={styles.stage}>*/}
+            <TableView>
+                <Section header="Sections" style={styles.container}>
+                    <SwipeListView
+                        disableRightSwipe
+                        data={sections?.map((cItem, i) => ({
+                            key: `${i}`,
+                            text: cItem?.name
+                        }))}
+                        renderItem={(data, rowMap) => (
+                            <TouchableHighlight
+                                onPress={() => console.log('switch pressed')}>
+                                <View style={styles.rowFront}>
+                                    <Text>{data.item.text}</Text>
+                                </View>
+                            </TouchableHighlight>
+                        )}
+                        renderHiddenRow={(data, rowMap) => (
+                            <View>
+                                <Text>Left</Text>
+                                <Text>Right</Text>
+                            </View>
+                        )}
+                        renderRow={(data, rowMap) => (
+                            <View>
+                                <Text>Left</Text>
+                                <Text>Right</Text>
+                            </View>
+                        )}
+                        renderHiddenItem={(data, rowMap) => (
+                            <View style={styles.rowBack}>
+                                <Text>Left</Text>
+                                <TouchableOpacity
+                                    style={[styles.backRightBtn, styles.backRightBtnRight]}
+                                    onPress={() => deleteSection(data.item.key)}
+                                >
+                                    <Text
+                                        style={styles.backTextWhite}>Delete</Text>
+                                </TouchableOpacity>
+                            </View>
+                        )}
+                        leftOpenValue={75}
+                        rightOpenValue={-75}
+                    />
+                </Section>
+            </TableView>
 
             {addASectionPressed &&
                 <KeyboardAvoidingView style={[styles.keyboardAvoidingViewStyle]}
@@ -158,6 +197,54 @@ const styles = StyleSheet.create({
         left: 0,
         right: 0,
         bottom: 0
+    },
+
+    switchContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginVertical: 50,
+        flexWrap: 'wrap',
+    },
+    switch: {
+        alignItems: 'center',
+        borderWidth: 1,
+        borderColor: 'black',
+        marginVertical: 2,
+        paddingVertical: 10,
+        width: Dimensions.get('window').width / 3,
+    },
+    rowFront: {
+        fontSize: 16,
+        height: 50,
+        paddingLeft: 16,
+        paddingRight: 16,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        marginBottom: 1
+    },
+    backTextWhite: {
+        color: '#FFF',
+    },
+    rowBack: {
+        alignItems: 'center',
+        backgroundColor: '#DDD',
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingLeft: 15,
+    },
+    backRightBtn: {
+        alignItems: 'center',
+        bottom: 0,
+        justifyContent: 'center',
+        position: 'absolute',
+        top: 0,
+        width: 75,
+        height: 50
+    },
+    backRightBtnRight: {
+        backgroundColor: 'red',
+        right: 0,
     },
 });
 
